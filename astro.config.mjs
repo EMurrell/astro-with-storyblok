2f8604cccb9da0104cdf0e65e41cd18a5750e887
EMurrell <ericmurrell@live.ca>
2023-11-21 16:39:35 -0500

    Add @astrojs/vercel package and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,37 +3,37 @@ import storyblok from "@storyblok/astro";
 import { loadEnv } from "vite";
 // add this for visual editor to work in dev mode:
 import basicSsl from "@vitejs/plugin-basic-ssl";
-
 const env = loadEnv("", process.cwd(), "STORYBLOK");
 // Since the Astro config file does not normally support environment variables, use the loadEnv function from Vite to load them.
 
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  // add this for visual editor to work in dev mode:
-  vite: {
-    plugins: [basicSsl()],
-    server: {
-      https: true,
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    bridge: env.STORYBLOK_IS_PREVIEW === "yes",
+    components: {
+      // Add your components here. The component paths are relative to the src directory. For example, if your component is located at src/storyblok/MyComponent.astro, the path would be storyblok/MyComponent (without the .astro extension).
+      blogPost: "storyblok/BlogPost",
+      blogPostList: "storyblok/BlogPostList",
+      page: "storyblok/Page"
     },
-  },
-  integrations: [
-    tailwind(),
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {
-        // Add your components here. The component paths are relative to the src directory. For example, if your component is located at src/storyblok/MyComponent.astro, the path would be storyblok/MyComponent (without the .astro extension).
-        blogPost: "storyblok/BlogPost",
-        blogPostList: "storyblok/BlogPostList",
-        page: "storyblok/Page",
-      },
-
-      apiOptions: {
-        // Choose your Storyblok space region
-        region: "us", // optional,  or 'eu' (default)
-      },
-    }),
-  ],
+    apiOptions: {
+      // Choose your Storyblok space region
+      region: "us" // optional,  or 'eu' (default)
+    }
+  }), tailwind()],
+  output: env.STORYBLOK_IS_PREVIEW === "yes" ? "server" : "static",
+  ...(env.STORYBLOK_ENV === "development" && {
+    vite: {
+      plugins: [basicSsl()],
+      server: {
+        https: true
+      }
+    }
+  }),
+  adapter: vercel()
 });
-
